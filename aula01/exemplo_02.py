79c29ee54415bf8c730816f9ddc9d385903afbc1
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2025-02-10 20:57:15 -0300

    Aula 01: conteudo tecnico

---
+++ aula01/exemplo_02.py
@@ -0,0 +1,3 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+print(len(input("Digite o seu nome: ")))
---
+++ aula01/exemplo_03.py
@@ -0,0 +1,6 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+print(num1 + num2)
